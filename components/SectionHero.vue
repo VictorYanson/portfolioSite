c60a17bf02dbf7fb94c78317058e3751200b4ae9
Victor Yanson <victor.yanson@outlook.com>
2025-07-28 18:05:55 +0200

    created custom button with animation and moved it to its own component file

--- components/SectionHero.vue
+++ components/SectionHero.vue
@@ -1,5 +1,11 @@
+<!-- <script setup>
+import { SeeRecentWorkButton } from '#components';
+</script> -->
+
 <template>
-    <div>
-        <div><p>heyyy</p></div>
+    <div class="m-10">
+        <div class="pseudo-border bg-gray-500 flex flex-row justify-center items-center center h-screen">
+            <SeeRecentWorkButton/>
+        </div>
     </div>
 </template>
---
+++ components/SeeRecentWorkButton.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="circle-container">
+        <Icon icon="solar:folder-bold" height="50" width="50"/>
+        <div class="see-recent-work absolute rotate-12 left-3 -top-16 w-40 h-12 bg-black flex items-center justify-center rounded-full">
+            <div class="relative flex justify-center">
+                <p class="text-white">See Recent Work</p>
+                <div class="absolute h-5 w-5 bg-black rotate-45 -bottom-4"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+.circle-container {
+    position: relative;
+    width: 150px;
+    height: 150px;
+    background-color: rgb(255, 255, 255, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 1000px;
+    cursor: pointer;
+    transition: background-color 0.4s ease-out;
+    backdrop-filter: blur(10px);
+    -webkit-backdrop-filter: blur(10px);
+}
+
+.circle-container:hover {
+    background-color: rgb(255, 255, 255, 0.8);
+}
+
+.see-recent-work {
+    transition: all 0.4s ease-in-out;
+}
+
+.circle-container:hover .see-recent-work {
+    left: 16px;
+    top: -70px;
+}
+</style>
